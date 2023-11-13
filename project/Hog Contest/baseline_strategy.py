17c9a8e0787c512c5fa6106cdee08da53c256119
infinityday <luyuhangcqu@163.com>
2023-11-13 20:08:07 +0800

    homework2

--- project/Hog Contest/baseline_strategy.py
+++
@@ -1,8 +0,0 @@
-"""
-    The strategy here will be compared with your final_strategy when you run
-    python3 compare_strategies.py. It will not be submitted as part of the contest.
-"""
-
-
-def baseline_strategy(score, opponent_score):
-    return 6
