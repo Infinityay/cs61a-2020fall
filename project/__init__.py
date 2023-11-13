58775206b9f7107401b49727fed2b5524cfc17ba
infinityday <luyuhangcqu@163.com>
2023-11-13 10:45:05 +0800

    Change the directory structure

--- project/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project : ML
-# @Time    : 2023/11/13 10:31
-# @Author  : infinityay
-# @File    : __init__.py.py
-# @Software: PyCharm 
-# @Contact me: https://github.com/Infinityay or stu.lyh@outlook.com
-# @Comment : 
