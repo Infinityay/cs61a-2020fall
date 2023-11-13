952cb2718d9bd95545fd4f194f96ab11b66ce518
infinityday <luyuhangcqu@163.com>
2023-11-13 10:37:53 +0800

    Change the directory structure

---
+++ project/Hog Contest/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Project : ML
+# @Time    : 2023/11/13 10:32
+# @Author  : infinityay
+# @File    : __init__.py.py
+# @Software: PyCharm 
+# @Contact me: https://github.com/Infinityay or stu.lyh@outlook.com
+# @Comment :
